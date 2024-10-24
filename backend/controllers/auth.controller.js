4a5b6a4f88105e6c28b4750a84540132a97f9c8b
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-10-24 22:24:54 +0530

    Sign UP

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+
+    // Check if the user already exists
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    });
+
+    await user.save();
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  res.send("Login Route");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout Route");
+};
