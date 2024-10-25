4d8919aba087dde69db47d7fe9e10b1f03ca7604
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-10-25 23:22:07 +0530

    Corrected

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const mailtrapClient = new MailtrapClient({
-  endpoint: process.env.MAILTRAP_TOKEN,
-  token: process.env.MAILTRAP_ENDPOINT,
+  endpoint: process.env.MAILTRAP_ENDPOINT,
+  token: process.env.MAILTRAP_TOKEN,
 });
 
 export const sender = {
   email: "hello@demomailtrap.com",
-  name: "Mailtrap Email",
+  name: "Mailtrap",
 };
